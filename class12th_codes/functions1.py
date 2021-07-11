f6a4bc8b0e2d82e02d7dda9a0f2c54c3b98837ac
Mohit Chattlani <mohchatt@publicisgroupe.net>
2021-07-11 19:31:37 +0530

    Added functions #1 lecture

---
+++ class12th_codes/functions1.py
@@ -0,0 +1,20 @@
+#function defination
+# def cubecompute(x):
+# 	return x**3
+
+# #calling a fn
+# cube_no=cubecompute(2)
+# print("Cube of num:"+str(cube_no))
+
+
+#P2 Program to add 2 nos
+
+#function defination
+def add(num1,num2):
+	sum_nos=num1+num2
+	return 1
+
+#function call
+sum_nos=add(2,3) #5
+
+print("Sum of nos is"+str(sum_nos))
