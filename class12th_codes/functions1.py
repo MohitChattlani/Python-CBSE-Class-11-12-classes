30862fdd21496b303b872700377267d116e2b3b3
Mohit Chattlani <mohchatt@publicisgroupe.net>
2021-08-14 18:17:50 +0530

    updated functions file

--- class12th_codes/functions1.py
+++ class12th_codes/functions1.py
@@ -12,9 +12,21 @@
 #function defination
 def add(num1,num2):
 	sum_nos=num1+num2
-	return 1
+	return sum_nos
 
-#function call
-sum_nos=add(2,3) #5
+#void fns
 
-print("Sum of nos is"+str(sum_nos))
+def printsomething():
+	print("Print something")
+
+
+printsomething()
+
+def main():
+	print(add(2,3))
+
+# #function call
+# sum_nos=add(2,3) #5
+
+# print("Sum of nos is"+str(sum_nos))
+
